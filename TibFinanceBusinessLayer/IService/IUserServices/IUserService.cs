44b7244515ed87bd088e70c0e6e1c0df8c694527
Abusaid <abusaid@nexkraft.com>
2023-09-22 17:32:01 +0600

    user service added

---
+++ TibFinanceBusinessLayer/IService/IUserServices/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TibFinanceDataAccess.Models;
+
+namespace TibFinanceBusinessLayer.IService.IUserServices
+{
+    public interface IUserService
+    {
+        User CreateUser(User user);
+        void UpdateRole(User user);
+        IEnumerable<User> GetAllRole();
+        User GetRoleById(int userId);
+        User DeleteRole(User user);
+    }
+}
---
+++ TibFinanceBusinessLayer/Services/UserServices/UserService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TibFinanceDataAccess.Models;
+using TibFinanceDataAccess.Repository.RolesRepository;
+using TibFinanceDataAccess.Repository.UserRepository;
+
+namespace TibFinanceBusinessLayer.Services.UserServices
+{
+    public class UserService
+    {
+        private readonly UserRepository userRepository;
+        public UserService(UserRepository userRepository)
+        {
+            userRepository = userRepository;
+        }
+        public User CreateUser(User user)
+        {
+            return userRepository.Create(user);
+        }
+        public bool DeleteUser(int id)
+        {
+            try
+            {
+                var user = userRepository.GetById(id);
+                userRepository.Delete(user);
+                return true;
+            }
+
+            catch (Exception)
+            {
+                return false;
+                throw;
+
+            }
+           
+        }
+        public IEnumerable<User> GetAllUsers()
+        {
+            try
+            {
+                return userRepository.GetAll().ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        public User GetRoleById(int? userId)
+        {
+            return userRepository.GetById(userId);
+            // var menu = _menuRepository.GetById(menuId);
+
+            //  throw new NotImplementedException();
+        }
+        public void UpdateRole(User user)
+        {
+            try
+            {
+                userRepository.Update(user);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            // throw new NotImplementedException();
+        }
+    }
+}
