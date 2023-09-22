272e18208298c6d2611cd56b592d9bf37b53e12a
Abusaid <abusaid@nexkraft.com>
2023-09-22 12:35:13 +0600

    role service integration started

---
+++ TibFinanceBusinessLayer/IService/IRoleServices/IRoleService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TibFinanceDataAccess.Models;
+
+namespace TibFinanceBusinessLayer.IService.IRoleServices
+{
+    public interface IRoleService
+    {
+        Role CreateRole(Role role);
+        void UpdateRole(Role role);
+        IEnumerable<Role> GetAllRole();
+        Role GetRoleById(int roleId);
+        Role DeleteRole(Role role);
+    }
+}
---
+++ TibFinanceBusinessLayer/Services/RoleServices/RoleServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TibFinanceDataAccess.Interface.Menus;
+using TibFinanceDataAccess.Interface.Modules;
+using TibFinanceDataAccess.Interface.Roles;
+using TibFinanceDataAccess.Models;
+using TibFinanceDataAccess.Repository.MenusRepository;
+using TibFinanceDataAccess.Repository.ModuleRepository;
+using TibFinanceDataAccess.Repository.RolesRepository;
+
+namespace TibFinanceBusinessLayer.Services.RoleServices
+{
+    public class RoleServices
+    {
+        private IRole roleRepository = null;
+        //private IMenu _menuRepository = null;
+        public RoleServices()
+        {
+            this.roleRepository = new RoleRepository();
+           
+        }
+        public Role CreateRoles(Role role)
+        {
+            return roleRepository.Create(role);
+        }
+    }
+}
